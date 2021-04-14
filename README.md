6aa87e2511914229af1b7eb3208cf2205de624f7
Dzheremilz <trempejeremie@gmail.com>
2021-04-14 17:56:20 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# exo-async-fast-copy
---
+++ fast-copy.js
@@ -0,0 +1,25 @@
+const fsPromises = require('fs/promises')
+
+const main = async () => {
+  // check src and dest are directory
+  let src = 'src/'
+  let dest = 'dest/'
+  let array = []
+  try {
+    const files = await fsPromises.readdir(src)
+    try {
+      // for of async ? for await of sync
+      for (const file of files) {
+        // if file copy, if directory create one, call the fct to copy files again ? recursively
+        array.push(fsPromises.copyFile(src + file, dest + file))
+      }
+      const result = await Promise.race(array)
+    } catch (e) {
+      console.log(e.message)
+    }
+  } catch (e) {
+    console.error(e.message)
+  }
+}
+
+main()
